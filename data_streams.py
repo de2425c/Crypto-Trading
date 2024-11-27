21f60a4cd182b3cc49c651f03b7159a1f7e452f8
de2425c <de2425@columbia.edu>
2024-11-27 16:19:07 -0500

    added gcloud functions

--- data_streams.py
+++ data_streams.py
@@ -4,79 +4,52 @@ import os
 from datetime import datetime
 import pytz
 from websockets import connect
-from termcolor import cprint
+from google.cloud import storage
 
-# Base WebSocket URLs
-websocket_url_base = 'wss://fstream.binance.com/ws/'
-trades_url = 'wss://fstream.binance.com/ws/'
-liquidation_url = 'wss://fstream.binance.com/ws/!forceOrder@arr'
-funding_url = 'wss://fstream.binance.com/ws/'
+trades_filename = '/tmp/recent_trades.csv'
+funding_filename = '/tmp/funding_rates.csv'
+liquidations_filename = '/tmp/liquidation_events.csv'
 
-# File paths for CSV storage
-recent_trades_file = 'recent_trades.csv'
-funding_rates_file = 'funding_rates.csv'
-liquidation_events_file = 'liquidation_events.csv'
+# Google Cloud Storage bucket name
+bucket_name = "crypto-sheets-usd"
 
-# Check and create files if they don't exist
 for filename, headers in [
-    (recent_trades_file, 'Event Time, Symbol, Trade ID, Price, Quantity, Trade Time, Is Buyer Maker\n'),
-    (funding_rates_file, 'Timestamp, Symbol, Funding Rate, Yearly Rate\n'),
-    (liquidation_events_file, 'Symbol, Side, Quantity, Price, USD Size, Event Time\n')
+    (trades_filename, 'Event Time, Symbol, Trade ID, Price, Quantity, Trade Time, Is Buyer Maker\n'),
+    (funding_filename, 'Timestamp, Symbol, Funding Rate, Yearly Rate\n'),
+    (liquidations_filename, 'Symbol, Side, Quantity, Price, USD Size, Event Time\n')
 ]:
     if not os.path.isfile(filename):
         with open(filename, 'w') as f:
             f.write(headers)
 
-# Global aggregator for trades
-class TradeAggregator:
-    def __init__(self):
-        self.trade_buckets = {}
+def upload_to_bucket(source_file_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+    print(f"{source_file_name} uploaded to {bucket_name}/{destination_blob_name}.")
 
-    async def add_trade(self, symbol, second, usd_size, is_buyer_maker):
-        trade_key = (symbol, second, is_buyer_maker)
-        self.trade_buckets[trade_key] = self.trade_buckets.get(trade_key, 0) + usd_size
-
-    async def check_and_print_trades(self):
-        timestamp_now = datetime.utcnow().strftime("%H:%M:%S")
-        deletions = []
-        for trade_key, usd_size in self.trade_buckets.items():
-            symbol, second, is_buyer_maker = trade_key
-            if second < timestamp_now and usd_size > 500000:
-                attrs = ['bold']
-                back_color = 'on_blue' if not is_buyer_maker else 'on_magenta'
-                trade_type = "BUY" if not is_buyer_maker else 'SELL'
-                usd_size /= 1_000_000  # Convert to millions
-                cprint(f"{trade_type} {symbol} {second} ${usd_size:.2f}m", 'white', back_color, attrs=attrs)
-                deletions.append(trade_key)
-
-        for key in deletions:
-            del self.trade_buckets[key]
-
-trade_aggregator = TradeAggregator()
-
-# Function to track recent trades
-async def track_trades(uri, filename, aggregator):
+async def track_trades(uri, filename):
     async with connect(uri) as websocket:
         while True:
             try:
                 message = await websocket.recv()
                 data = json.loads(message)
                 usd_size = float(data['p']) * float(data['q'])
-                trade_time = datetime.fromtimestamp(data['T'] / 1000, pytz.utc)
-                readable_trade_time = trade_time.strftime('%Y-%m-%d %H:%M:%S')
-
-                await aggregator.add_trade(data['s'], readable_trade_time, usd_size, data['m'])
+                if usd_size > 15000:
+                    trade_time = datetime.fromtimestamp(data['T'] / 1000, pytz.utc)
+                    readable_trade_time = trade_time.strftime('%Y-%m-%d %H:%M:%S')
 
                 # Write trade to CSV
-                with open(filename, 'a') as f:
-                    f.write(f"{data['E']}, {data['s']}, {data['a']}, {data['p']}, {data['q']}, {readable_trade_time}, {data['m']}\n")
+                    with open(filename, 'a') as f:
+                        f.write(f"{data['E']}, {data['s']}, {data['a']}, {data['p']}, {data['q']}, {readable_trade_time}, {data['m']}\n")
 
             except Exception as e:
+                print(f"Error in track_trades: {e}")
                 await asyncio.sleep(5)
 
-# Function to track funding rates
 async def track_funding_rates(symbol, filename):
-    uri = f"{funding_url}{symbol}@markPrice"
+    uri = f"wss://fstream.binance.com/ws/{symbol}@markPrice"
     async with connect(uri) as websocket:
         while True:
             try:
@@ -90,9 +63,9 @@ async def track_funding_rates(symbol, filename):
                     f.write(f"{timestamp}, {data['s']}, {funding_rate}, {yearly_rate}\n")
 
             except Exception as e:
+                print(f"Error in track_funding_rates: {e}")
                 await asyncio.sleep(5)
 
-# Function to track liquidation events
 async def track_liquidations(uri, filename):
     async with connect(uri) as websocket:
         while True:
@@ -111,25 +84,31 @@ async def track_liquidations(uri, filename):
                         f.write(f"{symbol}, {side}, {quantity}, {price}, {usd_size}, {timestamp}\n")
 
             except Exception as e:
+                print(f"Error in track_liquidations: {e}")
                 await asyncio.sleep(5)
 
-# Main function to run all tasks
+ 
 async def main():
-    symbols = ['btcusdt', 'solusdt',]
-    
-    # Tasks for recent trades
-    trade_tasks = [track_trades(f"{trades_url}{symbol}@aggTrade", recent_trades_file, trade_aggregator) for symbol in symbols]
-    
-    # Tasks for funding rates
-    funding_tasks = [track_funding_rates(symbol, funding_rates_file) for symbol in symbols]
-    
-    # Task for liquidation events
-    liquidation_task = track_liquidations(liquidation_url, liquidation_events_file)
-    
-    # Aggregated trade print task
-    print_task = asyncio.create_task(trade_aggregator.check_and_print_trades())
-    
-    await asyncio.gather(*trade_tasks, *funding_tasks, liquidation_task, print_task)
+    symbols = ['btcusdt', 'solusdt', 'ethusdt']
 
-# Run the unified script
+    trade_tasks = [track_trades(f"wss://fstream.binance.com/ws/{symbol}@aggTrade", trades_filename) for symbol in symbols]
+    funding_tasks = [track_funding_rates(symbol, funding_filename) for symbol in symbols]
+    liquidation_task = track_liquidations("wss://fstream.binance.com/ws/!forceOrder@arr", liquidations_filename)
+
+    async def upload_files_periodically():
+        while True:
+            try:
+                upload_to_bucket(trades_filename, "recent_trades.csv")
+                upload_to_bucket(funding_filename, "funding_rates.csv")
+                upload_to_bucket(liquidations_filename, "liquidation_events.csv")
+                print("Files uploaded successfully.")
+            except Exception as e:
+                print(f"Error uploading files: {e}")
+            await asyncio.sleep(3600)  # Upload every hour
+
+    upload_task = asyncio.create_task(upload_files_periodically())
+    await asyncio.gather(*trade_tasks, *funding_tasks, liquidation_task, upload_task)
+
 asyncio.run(main())
+
+
